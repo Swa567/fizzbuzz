a76d7ff29564f2d5dc246df032b55a077edb70bb
j-cort <81960011+j-cort@users.noreply.github.com>
2021-11-09 15:38:03 +0000

    refactor fb production code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,19 @@
 def fizzbuzz(number)
-  if number % 15 == 0
-    "fizzbuzz"
-  elsif number % 3 == 0
-    "fizz"
-  elsif number % 5 == 0
-    "buzz"
-  else
-    number
-  end
+  number % 15 == 0 ? "fizzbuzz"
+    : number % 3 == 0 ? "fizz"
+    : number % 5 == 0 ? "buzz"
+    : number
 end
+
+# def fizzbuzz(number)
+#   if number % 15 == 0
+#     "fizzbuzz"
+#   elsif number % 3 == 0
+#     "fizz"
+#   elsif number % 5 == 0
+#     "buzz"
+#   else
+#     number
+#   end
+# end
+
